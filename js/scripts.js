c551a9118cb95a280a63c347e680cdb0da2c687c
rrichardknott <67433117+rrichardknott@users.noreply.github.com>
2020-07-13 15:34:51 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+﻿
+    $("#errorAlert").hide();
+    $("#errorAlertButton").hide();
+
+    $("#submitButton").click(function () {
+        if ($("#phrase").val().length == 0) {
+            $("#errorAlertButton").show();
+            $("#submitButton").hide();
+            $("#clearButton").hide();
+            $("#errorAlert").show();
+            return;
+        }
+
+        let lettersOnly = /[\W0-9_]/g;
+        let cleanPhrase = $("#phrase").val().toLowerCase().replace(lettersOnly, "");
+        let reversedPhrase = cleanPhrase.split("").reverse().join("");
+
+        $("#resultText").val(reversedPhrase);
+
+        if (cleanPhrase === reversedPhrase) {
+            $("#title").text("It's a Palindrome!");
+        } else {
+            $("#title").text("It's not a Palindrome.");
+        }
+    });
+
+    $("#errorAlertButton").on("click", function () {
+        $("#errorAlertButton").hide();
+        $("#phrase").val("").focus();
+        $("#submitButton").show();
+        $("#clearButton").show();
+        $("#title").text("taco cat");
+        $("#resultText").val("");
+        $("#errorAlert").hide();
+    });
+
+    $("#clearButton").on("click", function () {
+        $("#phrase").val("").focus();
+        $("#title").text("taco cat");
+        $("#resultText").val("");
+    });
